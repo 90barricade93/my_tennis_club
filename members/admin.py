c032ad316b0272c3879e95a8fd1bfd442ed96099
Raymond <raydevries76@gmail.com>
2023-02-17 21:19:58 +0100

    added membership number at database for members

--- members/admin.py
+++ members/admin.py
@@ -4,6 +4,6 @@ from .models import Member
 # Register your models here.
 
 class MemberAdmin(admin.ModelAdmin):
-  list_display = ("firstname", "lastname", "joined_date", "double_player",)
+  list_display = ("firstname", "lastname", "joined_date", )
   
 admin.site.register(Member, MemberAdmin)
--- members/models.py
+++ members/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Member(models.Model):
+  membership_number = models.IntegerField(null=True)
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
